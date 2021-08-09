1b7da9f75ad41216e0cf3002383fa29e68a56604
Jean-Baptiste PERIN <jean-baptiste.perin@orange.com>
2021-08-09 14:39:45 +0200

    get the maximum free space

--- src/osdk_config.bat
+++ src/osdk_config.bat
@@ -3,8 +3,8 @@
 ::
 :: Set the build paremeters
 ::
-SET OSDKADDR=$500	
-SET OSDKNAME=AYRAMP
+SET OSDKADDR=$400	
+SET OSDKNAME=GROOVE
 SET OSDKFILE=main ay_s kernel keyboard keybuf_s soundbuf
 :: SET OSDKDISK=%OSDKNAME%.dsk
 
